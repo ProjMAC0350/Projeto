69858306285ddf5857bb0f26af1da672fdc4ff32
Fernando Lima <fernandolimabusiness@gmail.com>
2024-07-02 07:35:35 -0300

    fixed register

--- frontend/src/Pages/Registerpage.js
+++ frontend/src/Pages/Registerpage.js
@@ -2,58 +2,71 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 
-
 export const Register = () => {
-  const [usuarios, setFormUsuarios] = useState({
-    name: '',
-    nusp: '',
-    email: '',
-    pwd: '',
-    photo: '',
-    course: '',
-  });
+  const [name, setName] = useState('');
+  const [nusp, setNusp] = useState('');
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [photo, setPhoto] = useState('');
+  const [course, setCourse] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const mudarpag = useNavigate();
 
   const handleChange = (event) => {
     const { name, value, type, files } = event.target;
-    setFormUsuarios({
-        ...usuarios,
-        [name]: type === 'file' ? files[0] : value,
-    });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'nusp':
+        setNusp(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'pwd':
+        setPwd(value);
+        break;
+      case 'photo':
+        setPhoto(value);
+        break;
+      case 'course':
+        setCourse(value);
+        break;
+      default:
+        break;
+    }
   };
-
-  const [redirect, setRedirect] = useState(false);
 
   const handleSubmit = async (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
 
-    setFormUsuarios.append('name', setFormUsuarios.name);
-    setFormUsuarios.append('nusp', setFormUsuarios.nusp);
-    setFormUsuarios.append('email', setFormUsuarios.email);
-    setFormUsuarios.append('pwd', setFormUsuarios.pwd);
-    setFormUsuarios.append('photo', setFormUsuarios.photo);
-    setFormUsuarios.append('course', setFormUsuarios.course);
+    const dados = new FormData();
+    dados.append('name', name);
+    dados.append('nusp', nusp);
+    dados.append('email', email);
+    dados.append('pwd', pwd);
+    dados.append('photo', photo);
+    dados.append('course', course);
 
-    try{
-      const response = await axios.post('http://localhost:8080/users', usuarios, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    try {
+      const response = await axios.post('http://localhost:8080/users', dados, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
       });
 
-      if(response.status === 201){
-        alert('Usuario Criado');
+      if (response.status === 201) {
+        alert('Usuário criado com sucesso');
         setRedirect(true);
-      } 
-      else{
-          alert("Usuario nao foi criado");
+      } else {
+        alert('Usuário não foi criado');
       }
-    } 
-    catch(error){
-      alert("Erro");
-      }
+    } catch (error) {
+      alert('Erro ao cadastrar usuário');
+      console.error(error);
+    }
   };
-
-  const mudarpag = useNavigate();
 
   useEffect(() => {
     if (redirect) {
@@ -61,31 +74,32 @@ export const Register = () => {
     }
   }, [redirect, mudarpag]);
 
-  function Mouseover(event) {
+  const handleMouseOver = (event) => {
     event.target.style.background = 'rgb(300, 190, 2)';
-  }
-  function Mouseout(event){
-    event.target.style.background="";
-  }
-      
-  return ( 
+  };
+
+  const handleMouseOut = (event) => {
+    event.target.style.background = '';
+  };
+
+  return (
     <div>
       <div className="headerlogin">
-         <div className="headerline">
-            <div id="HeaderUSP"></div>
-         </div>
+        <div className="headerline">
+          <div id="HeaderUSP"></div>
+        </div>
       </div>
       <div className="loginpage">
         <div className="Blocoregister">
           <div className="contentloginBloco">
             <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Nome" name='name' onChange={handleChange}/>
-            <input type="number" placeholder="Número Usp" name='nusp' onChange={handleChange}/>
-            <input type="text" placeholder="Email" name='email' onChange={handleChange}/>
-            <input type='Password' placeholder='Senha' name='pwd' onChange={handleChange}/>
-            <input type="text" placeholder="Link da foto" name='photo' onChange={handleChange}/>
-            <input type="Text" placeholder="Curso" name='course' onChange={handleChange}/>
-            <Link to="/"><button className="buttonlogin" type="submit" onMouseOver={Mouseover} onMouseOut={Mouseout}>Registrar</button></Link>
+              <input type="text" placeholder="Nome" name='name' value={name} onChange={handleChange} />
+              <input type="number" placeholder="Número Usp" name='nusp' value={nusp} onChange={handleChange} />
+              <input type="text" placeholder="Email" name='email' value={email} onChange={handleChange} />
+              <input type='password' placeholder='Senha' name='pwd' value={pwd} onChange={handleChange} />
+              <input type="text" placeholder="Link da foto" name='photo' value={photo} onChange={handleChange} />
+              <input type="text" placeholder="Curso" name='course' value={course} onChange={handleChange} />
+              <button className="buttonlogin" type="submit" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Registrar</button>
             </form>
           </div>
         </div>
